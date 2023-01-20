01e08293d4d0de3b400d6e384f6365530cacbacb
Iago Abner <iagoabner.dev@gmail.com>
2023-01-19 22:53:50 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
+import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [ fontsLoading ]=useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_800ExtraBold
+  })
+
+  if(!fontsLoading){
+    return <Loading />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>TESTANDO</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +26,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'Inter_800ExtraBold',
+    color: 'white'
+  }
 });
