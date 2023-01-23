98ea36ac86d47c43dbe4b1a41567d0a13d8a7f97
Iago Abner <iagoabner.dev@gmail.com>
2023-01-23 18:13:29 -0300

    feat: add spinner to loading fonts

--- App.tsx
+++ App.tsx
@@ -1,37 +1,25 @@
 import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Loading } from './src/components/Loading';
+import './src/lib/dayjs';
+import { Home } from './src/screens/Home';
 
 export default function App() {
-  const [ fontsLoading ]=useFonts({
-    Inter_400Regular, 
-    Inter_600SemiBold, 
-    Inter_700Bold, 
+  const [fontsLoading] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
     Inter_800ExtraBold
   })
 
-  if(!fontsLoading){
+  if (!fontsLoading) {
     return <Loading />
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>TESTANDO</Text>
+    <>
       <StatusBar style="light" />
-    </View>
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontFamily: 'Inter_800ExtraBold',
-    color: 'white'
-  }
-});
